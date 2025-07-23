13b3e95ad37abf1a1ccddd8cc1c573c57250de70
Riviets <vladvladpobuta@gmail.com>
2025-07-23 09:28:13 +0300

    Set up the environment for backend and frontend. Implemented backend logic

---
+++ todolist-frontend/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <p className="text-red-500 font-bold text-3xl text-center">
+        Everything works
+      </p>
+    </>
+  );
+}
+
+export default App;

8f2cdc366338970b43d7163c52aa3dace0db7f69
Riviets <vladvladpobuta@gmail.com>
2025-08-06 21:47:37 +0300

    Improved the data fetching for today's assignments to avoid unnecessary rerenders

--- todolist-frontend/src/components/UserAssignmentsForToday.tsx
+++ todolist-frontend/src/components/UserAssignmentsForToday.tsx
@@ -1,25 +1,16 @@
-import { useEffect, useState } from "react";
 import { useCurrentUserId } from "../hooks/useCurrentUserId";
-import type { Assignment } from "../types/assignment";
 import { assignmentsService } from "../services/api/assignmentsService";
+import type { Assignment } from "../types/assignment";
 import AssignmentCard from "./AssignmentCard";
+import { useQuery } from "@tanstack/react-query";
 
 const UserAssignmentsForToday = () => {
   const { userId } = useCurrentUserId();
-  const [assignmentsForToday, setAssignmentsForToday] = useState<
-    Assignment[] | null
-  >(null);
-  useEffect(() => {
-    const setAssignments = async () => {
-      if (userId) {
-        const assignments = await assignmentsService.getUserAssignmentsForToday(
-          userId!
-        );
-        setAssignmentsForToday(assignments);
-      }
-    };
-    setAssignments();
-  }, [userId]);
+  const { data: assignmentsForToday } = useQuery({
+    queryKey: ["userAssignmentsForToday"],
+    queryFn: () => assignmentsService.getUserAssignmentsForToday(userId!),
+    enabled: userId !== null,
+  });
   return (
     <div>
       {assignmentsForToday?.length === 0 ? (
@@ -32,7 +23,7 @@ const UserAssignmentsForToday = () => {
             Your Assignments for <span className="underline">today:</span>
           </p>
           <div className="flex gap-4 overflow-x-auto p-3 mb-14 shadow-md">
-            {assignmentsForToday?.map((assignment) => (
+            {assignmentsForToday?.map((assignment: Assignment) => (
               <AssignmentCard
                 assignment={assignment}
                 className="min-w-[250px]"

2c82e446df5711a0b48649e99f36f023f19c88e0
Riviets <vladvladpobuta@gmail.com>
2025-08-15 15:39:25 +0300

    Prepared the project for deployment

---
+++ todolist-backend-adonis/database/migrations/1754717841045_create_assignments_table.ts
@@ -0,0 +1,21 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'assignments'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.integer('user_id').notNullable().references('id').inTable('users').onDelete('CASCADE')
+      table.string('title').notNullable()
+      table.date('appointed_date').notNullable()
+
+      table.timestamp('created_at').defaultTo(this.now())
+      table.timestamp('updated_at').defaultTo(this.now())
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
--- todolist-backend-adonis/database/migrations/1755256827833_create_tasks_table.ts
+++ todolist-backend-adonis/database/migrations/1755256827833_create_tasks_table.ts
@@ -1,17 +1,13 @@
 import { BaseSchema } from '@adonisjs/lucid/schema'
 
 export default class extends BaseSchema {
-  protected tableName = 'assignments'
+  protected tableName = 'tasks'
 
   async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-      table.integer('user_id').notNullable().references('id').inTable('users').onDelete('CASCADE')
       table.string('title').notNullable()
-      table.date('appointed_date').notNullable()
-
-      table.timestamp('created_at')
-      table.timestamp('updated_at')
+      table.boolean('completed').defaultTo(false)
     })
   }
 
